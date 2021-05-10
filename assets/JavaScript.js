13a4eea4ec35f501f223ee01866ad68a1a0e7781
Ryan Blessing <ryanblessing20@yahoo.com>
2021-05-10 18:00:16 -0500

    timer function

--- assets/JavaScript.js
+++ assets/JavaScript.js
@@ -10,7 +10,7 @@
 //my targets connecting me to HTML
 var startBtn = document.getElementById('start');
 var timerEl = document.getElementById('timer');
-var questions = [
+var questionsEl = [
     {q: 'The DOM is built into the JavaScript Language.', a: 't'},
     {q: 'event.preventDefault() doesnt stop your page from resetting up submission.', a: 'f'},
     {q: 'The DOM event object refers to the HTML element that was interacted with to dispatch the event.', a: 't'},
@@ -24,6 +24,7 @@ var score = 0;
 function startBtn() {
 for (var i = 0; i < questions.length; i++) {
     var answer = confirm(questions[i].q);
+    console.log(i);
 
     if (
         (answer === true && questions[i].a === 't') ||
@@ -38,36 +39,29 @@ for (var i = 0; i < questions.length; i++) {
 
 //timer for quiz
 function timer() {
-    var timeLeft = 90;
+    var timeLeft = 60;
 
-    var timeInterval = setInterval(function(){
-        if (timeLeft >= 1){
-            timerEl.textContent = timeLeft + 'time remaining'
+    var timeInterval = setInterval(function() {
+        
+        if (timeLeft >= 1) {
+            timerEl.textContent = timeLeft + 'time remaining';
             timeLeft--;
+        } 
+        else if(timeLeft === 0) {
+            timerEl.textContent = timeLeft = 'No Time Remaining';
+            timeLeft--; 
         }
-        else{
-            timerEl.textContent = ' '
-            clearInterval(timeInterval)
+        else {
+            timerEl.textContent = '';
+            clearInterval(timeInterval);
             displayMessage();
             }
     },1000);
 }
+
 //message after your done with quiz
 var message =
-'Congradulatiuons on completing this quiz! hope you had fun!!'
+'Congradulations on completing this quiz! hope you had fun!!'
 var words = message.split(' ');
 
- 
-
-// questions for the quiz
-
-// True or False
-//1.)DOM is built into the JavaScript Language-T
-//2.)event.preventDefault() doesnt stop your page from resetting up submission-F
-//3.The DOm event object refers to the HTML element that was interacted
-// with to dispatch the event-T
-//4. Local.storage() does support saving arrays-F
-
-
-// click button to start timer
 startBtn.onclick = timer;
